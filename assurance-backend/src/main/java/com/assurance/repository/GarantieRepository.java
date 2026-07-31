aa1cc810f9e5c84f2ba009828d2fdec7eaefd985
Anas <anas.oussous11@gmail.com>
2026-07-31 16:00:24 +0100

    changes

--- assurance-backend/src/main/java/com/assurance/repository/GarantieRepository.java
+++ assurance-backend/src/main/java/com/assurance/repository/GarantieRepository.java
@@ -1,10 +1,17 @@
 package com.assurance.repository;
 
 import com.assurance.entity.Garantie;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GarantieRepository extends JpaRepository<Garantie, Long> {
+    @Override
+    @EntityGraph(attributePaths = "groupeExclusion")
+    List<Garantie> findAll(Sort sort);
+
     Optional<Garantie> findByCode(String code);
 }
